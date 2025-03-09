90b25411eb73a0fe0076acf7a2a1ceb3b0f001ac
Nikolai <n91056379@gmail.com>
2025-03-09 17:12:06 +0100

    add all locators

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+    private final By CLICK_BUTTON_PRESENT = By.xpath("//div[@id='wrap-close-button-1454703513200']");
+    private final By CLICK_BUTTON_PASSWORD = By.xpath("//input[@id='LoginModel_Password']");
+    private final By CLICK_FIELD_EMAIL = By.xpath("//input[@id='LoginModel_Email']");
+    private final By CLICK_BUTTON_ADVERTISING = By.xpath("//div[@id='wrap-button-1601498479230']");
+
+
+}
