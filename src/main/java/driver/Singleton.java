8db8598cdff15137922ae59d60f2303a19ddd68c
Nikolai <n91056379@gmail.com>
2025-03-09 16:11:34 +0100

    add class Singleton

---
+++ src/main/java/driver/Singleton.java
@@ -0,0 +1,34 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+    public class Singleton {
+        private static WebDriver driver;
+
+        private Singleton() {
+
+        }
+
+        public static WebDriver getDriver() {
+            if (driver == null) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications");
+                driver = new ChromeDriver(options);
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+                driver.manage().window().maximize();
+
+            }
+            return driver;
+        }
+
+        public static void quit() {
+            if (driver != null) {
+                driver.quit();
+                driver = null;
+            }
+        }
+    }
