7837179b213a72774e3e8d59df9ea61e3e3f828d
Nikolai <n91056379@gmail.com>
2025-03-09 17:08:27 +0100

    refactor  method and locator

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -11,12 +11,12 @@ public class HomePage {
     private final By CLICK_TITLE_ENTER = By.xpath("(//span[text()='Войти'])[2]");
     private final By CLICK_DROP_BUTTON_ENTER = By.xpath("//button[@class='button header__section-desktop']");
     private final By CLICK_TITLE_BAG = By.xpath("//span[text()='Корзина']");
-    private final By CLICK_IMG = By.xpath("(//img[@class='yall-loaded'])[1]");
+    private final By CLICK_IMG = By.xpath("(//img[@class='yall-loaded'])[3]");
     private final By SEARCH = By.xpath("(//input[@placeholder='Поиск: Джинсы, Боди, Футболки, Спортивные костюмы '])[2]");
     private final By SEARCH_CLICK = By.xpath("(//span[text()='Поиск']");
     private final By SEARCH_INPUT_CLICK = By.xpath("//input[@class='search-button main-search-button']");
 
-    private String URL = "https://www.defacto.com/ru-ua";
+    private String URL = "https://www.defacto.com/ru-kz";
 
     private WebDriver driver;
 
@@ -50,6 +50,8 @@ public class HomePage {
     }
 
     public HomePage clickImg() {
+        Actions action = new Actions(driver);
+        action.scrollByAmount(0,1000).perform();
         driver.findElement(CLICK_IMG).click();
         return this;
     }
