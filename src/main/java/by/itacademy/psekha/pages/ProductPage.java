2baef6b2b8d01b1b482feed477de1fe2c72461c7
Nikolai <n91056379@gmail.com>
2025-03-13 17:32:49 +0100

    add class ProductPage

---
+++ src/main/java/by/itacademy/psekha/pages/ProductPage.java
@@ -0,0 +1,48 @@
+package by.itacademy.psekha.pages;
+
+import by.itacademy.psekha.driver.Singleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ProductPage {
+    private final By PRODUCT_ADD = By.xpath("//div[@data-swiper='product-card__swiper-1']");
+
+    private WebDriver driver;
+
+    public ProductPage() {
+        this.driver = Singleton.getDriver();
+    }
+
+    private static final Logger logger = LogManager.getLogger(ProductPage.class);
+
+    public ProductPage scrollSite() {
+        Actions action = new Actions(driver);
+        try {
+            action.scrollByAmount(0, 250).perform();
+            return this;
+        } catch (Exception e) {
+            logger.info("Attempt to scroll the page");
+            logger.error("An error occurred while scrolling the page", e);
+            throw e;
+        }
+    }
+
+    public ProductPageAddBag clickProductPage() {
+        try {
+            driver.findElement(PRODUCT_ADD).click();
+            return new ProductPageAddBag();
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click but element not found.");
+            logger.error("Element to click not found");
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click but an error occurred.");
+            logger.error("An error occurred while attempting to click.");
+            throw e;
+        }
+    }
+}
