785df8b8d80f518ed62e669efbfd9d8ddc06230d
Nikolai <n91056379@gmail.com>
2025-03-13 17:31:37 +0100

    add LoginTest

---
+++ src/test/java/by/itacademy/psekha/uitest/LoginTest.java
@@ -0,0 +1,50 @@
+package by.itacademy.psekha.uitest;
+
+import by.itacademy.psekha.pages.HomePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LoginTest {
+    @Test
+    @DisplayName("Login test with empty Email ")
+    public void test1() {
+        String actual = new HomePage()
+                .open()
+                .clickTitleEnter()
+                .clickDropButtonEnter()
+                .writePassword("23223")
+                .clickLoginEnterButton()
+                .getTextErrorEmail();
+        Assertions.assertEquals("Пожалуйста, введите адрес электронной почты", actual);
+
+    }
+
+    @Test
+    @DisplayName("Login test with empty password")
+    public void test2() {
+        String actual = new HomePage()
+                .open()
+                .clickTitleEnter()
+                .clickDropButtonEnter()
+                .writeEmail("ppsekha@mail.ru")
+                .clickLoginEnterButton()
+                .getTextErrorPassword();
+        Assertions.assertEquals("Пароль должен быть от 6 до 10 символов.", actual);
+
+    }
+
+    @Test
+    @DisplayName("Login test with full fields")
+    public void test3() {
+        String actual = new HomePage()
+                .open()
+                .clickTitleEnter()
+                .clickDropButtonEnter()
+                .writeEmail("ppsekha@mail.ru")
+                .writePassword("12589")
+                .clickLoginEnterButton()
+                .getTextError();
+        Assertions.assertEquals("Пожалуйста, проверьте информацию пользователя и попробуйте снова", actual);
+    }
+}
