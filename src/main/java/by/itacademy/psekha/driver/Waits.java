04d722e71cc7499be45771d6c8131fe2521621aa
Nikolai <n91056379@gmail.com>
2025-03-13 13:21:18 +0100

    add waits class

---
+++ src/main/java/by/itacademy/psekha/driver/Waits.java
@@ -0,0 +1,13 @@
+package by.itacademy.psekha.driver;
+
+public class Waits {
+    public void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
