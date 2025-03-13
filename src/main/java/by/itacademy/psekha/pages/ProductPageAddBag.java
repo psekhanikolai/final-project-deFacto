57f0309aebfb722641326a9d0fc5e2f5bd9c689f
Nikolai <n91056379@gmail.com>
2025-03-13 17:30:06 +0100

    add class ProductPageAddBag with logger

---
+++ src/main/java/by/itacademy/psekha/pages/ProductPageAddBag.java
@@ -0,0 +1,65 @@
+package by.itacademy.psekha.pages;
+
+import by.itacademy.psekha.driver.Singleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPageAddBag {
+    private final By PRODUCT_ADD_BAG = By.xpath("(//span[text()='Добавить в корзину'])[1]");
+    private final By PRODUCT_ADD_L = By.xpath("//button[@value='L']");
+    private final By CLICK_TITLE_BAG = By.xpath("//span[text()='Корзина']");
+
+    private WebDriver driver;
+
+    public ProductPageAddBag() {
+        this.driver = Singleton.getDriver();
+    }
+
+    private static final Logger logger = LogManager.getLogger(ProductPageAddBag.class);
+
+    public ProductPageAddBag clickProductAddBag() {
+        try {
+            driver.findElement(PRODUCT_ADD_BAG).click();
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click but element not found.");
+            logger.error("Element to click not found");
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click but an error occurred.");
+            logger.error("An error occurred while attempting to click.");
+            throw e;
+        }
+    }
+    public ProductPageAddBag clickProductAddL() {
+        try {
+            driver.findElement(PRODUCT_ADD_L).click();
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click but element not found.");
+            logger.error("Element to click not found");
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click but an error occurred.");
+            logger.error("An error occurred while attempting to click.");
+            throw e;
+        }
+    }
+    public ProductPageAddBag clickTitleBag() {
+        try {
+            driver.findElement(CLICK_TITLE_BAG).click();
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click but element not found.");
+            logger.error("Element to click not found");
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click but an error occurred.");
+            logger.error("An error occurred while attempting to click.");
+            throw e;
+        }
+    }
+}
