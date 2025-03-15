79b27d1e0f42c2f41dbcd3fd01eb59a4c2cd2aca
Nikolai <n91056379@gmail.com>
2025-03-15 18:52:05 +0100

    add ApiTest

--- src/test/java/by/itacademy/psekha/apitest/ApiTest.java
+++ src/test/java/by/itacademy/psekha/apitest/ApiTest.java
@@ -2,15 +2,18 @@ package by.itacademy.psekha.apitest;
 
 import by.itacademy.psekha.api.ApiPage;
 import by.itacademy.psekha.api.ReturnBody;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import jdk.jfr.Description;
 import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTest {
 
     @Test
-    @DisplayName("Get site")
+    @Description("Get site")
     public void test() {
         Response response = ApiPage.getDoSite();
         response.then()
@@ -18,7 +21,7 @@ public class ApiTest {
     }
 
     @Test
-    @DisplayName("Post login")
+    @Description("Post login")
     public void test1() {
         Response response = ApiPage.postDoLogin();
         response.then().log().all();
@@ -32,7 +35,7 @@ public class ApiTest {
     }
 
     @Test
-    @DisplayName("Post addcart")
+    @Description("Post addcart")
     public void test2() {
         Response response = ApiPage.postDoAddCart();
         response.then()
@@ -42,7 +45,7 @@ public class ApiTest {
     }
 
     @Test
-    @DisplayName("add cart")
+    @Description("add cart")
     public void test4() {
         Response response = ApiPage.postDoAddCart1();
         ReturnBody returnBody = response.getBody().as(ReturnBody.class);
@@ -51,5 +54,29 @@ public class ApiTest {
         softAssertions.assertThat(statusCode).isEqualTo(200);
         softAssertions.assertThat(returnBody.Data).isEqualTo("Товар был добавлен в Вашу корзину");
         softAssertions.assertAll();
+    }
+
+    @Test
+    @Description("Empty password field")
+    public void testLogin() {
+        String response = ApiPage.postDoLoginEmptyPassword()
+                .then()
+                .extract()
+                .asString();
+        JsonPath jsonPath = new JsonPath(response);
+        String passwordEmpty = jsonPath.getString("ValidationErrors[0].v");
+        assertThat(passwordEmpty, equalTo("Пожалуйста, введите Ваш пароль "));
+    }
+
+    @Test
+    @Description("Empty Email field")
+    public void testLogin1() {
+        String response = ApiPage.postDoLoginEmptyEmail()
+                .then()
+                .extract()
+                .asString();
+        JsonPath jsonPath = new JsonPath(response);
+        String emailEmpty = jsonPath.getString("ValidationErrors[0].v");
+        assertThat(emailEmpty, equalTo("Пожалуйста, введите адрес электронной почты"));
     }
 }
