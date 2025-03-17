66e4a33437c514f2e76b22af64cc3ed415b3aad0
Nikolai <n91056379@gmail.com>
2025-03-17 15:28:35 +0100

    refactor HomePage

--- src/main/java/by/itacademy/psekha/pages/HomePage.java
+++ src/main/java/by/itacademy/psekha/pages/HomePage.java
@@ -15,7 +15,7 @@ public class HomePage {
     private final By CLICK_TITLE_ENTER = By.xpath("(//span[text()='Войти/Зарегистрироваться'])[2]");
     private final By CLICK_DROP_BUTTON_ENTER = By.xpath("//button[@class='button header__section-desktop']");
     private final By CLICK_TITLE_BAG = By.xpath("//span[text()='Корзина']");
-    private final By CLICK_IMG = By.xpath("(//img[@class='yall-loaded'])[3]");
+    private final By CLICK_IMG = By.xpath("(//img[@class='yall-loaded'])[2]");
     private final By SEARCH = By.xpath("(//input[@placeholder='Поиск: Джинсы, Боди, Футболки, Спортивные костюмы '])[2]");
     private final By SEARCH_CLICK = By.xpath("(//span[text()='Поиск']");
     private final By SEARCH_INPUT_CLICK = By.xpath("//input[@class='search-button main-search-button']");
@@ -82,7 +82,7 @@ public class HomePage {
     public HomePage scrollSite() {
         Actions action = new Actions(driver);
         try {
-            action.scrollByAmount(0, 1000).perform();
+            action.scrollByAmount(0, 900).perform();
             return this;
         } catch (Exception e) {
             logger.info("Attempt to scroll the page");
@@ -92,10 +92,10 @@ public class HomePage {
     }
 
     @Step
-    public HomePage clickImg() {
+    public ProductPage clickImg() {
         try {
             driver.findElement(CLICK_IMG).click();
-            return this;
+            return new ProductPage();
         } catch (NoSuchElementException e) {
             logger.info("Attempted to click but element not found.");
             logger.error("Element to click not found");
