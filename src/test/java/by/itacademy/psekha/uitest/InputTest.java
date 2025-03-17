9afc87d46f237b1bd0f822426ebc921626678c0d
Nikolai <n91056379@gmail.com>
2025-03-17 15:25:10 +0100

    add AfterEach

--- src/test/java/by/itacademy/psekha/uitest/InputTest.java
+++ src/test/java/by/itacademy/psekha/uitest/InputTest.java
@@ -1,13 +1,18 @@
 package by.itacademy.psekha.uitest;
 
+import by.itacademy.psekha.driver.Singleton;
+import by.itacademy.psekha.pages.BagPage;
 import by.itacademy.psekha.pages.HomePage;
+import by.itacademy.psekha.pages.ProductPage;
+import by.itacademy.psekha.pages.ProductPageAddBag;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputTest {
     @Test
-    @DisplayName("Write and search text")
+    @Description("Write and search text")
     public void test(){
        String actual = new HomePage()
                .open()
@@ -15,5 +20,29 @@ public class InputTest {
                .writeFieldSearch("Рубашка")
                .getTitleTextProduct();
         Assertions.assertEquals("‘Рубашка’ Результаты поиска:", actual);
+    }
+
+    @Test
+    @Description("")
+    public void test1(){
+       ProductPage hp = new HomePage().open().clickTitleMan().scrollSite().clickImg().scrollSite1();
+    }
+
+    @Test
+    public void test2(){
+        BagPage bp = new HomePage()
+                .open()
+                .clickTitleMan()
+                .scrollSite()
+                .clickImg().scrollSite1()
+                .clickProductPage()
+                .clickProductAddL()
+                .clickProductAddBag()
+                .clickTitleBag();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Singleton.quit();
     }
 }
