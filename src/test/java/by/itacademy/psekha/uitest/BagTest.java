06af5b85007eda561522b0efa46e06a295caf3ff
Nikolai <n91056379@gmail.com>
2025-03-17 15:24:54 +0100

    add AfterEach

--- src/test/java/by/itacademy/psekha/uitest/BagTest.java
+++ src/test/java/by/itacademy/psekha/uitest/BagTest.java
@@ -1,17 +1,24 @@
 package by.itacademy.psekha.uitest;
 
+import by.itacademy.psekha.driver.Singleton;
 import by.itacademy.psekha.pages.BagPage;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BagTest {
     @Test
-    @DisplayName("Open and text checking")
+  @Description("Open and text checking")
     public void test(){
         String actual = new BagPage()
                 .openBag()
                 .getTitleText();
         Assertions.assertEquals("Ваша корзина пуста.", actual);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Singleton.quit();
     }
 }
